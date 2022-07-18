3c5e919454287cc8ed6dcf7f309c2867de621a1a
tce-thirumani <88088672+tce-thirumani@users.noreply.github.com>
2022-07-19 01:56:46 +0530

    basic crud operations

--- angular-basics/src/app/components/home/home.component.ts
+++ angular-basics/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudHttpService } from 'src/app/services/crud-http.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private crudHttpService: CrudHttpService) { }
+  todoList:any = [];
   ngOnInit(): void {
+    this.listTodos();
+  }
+
+  listTodos(){
+    this.crudHttpService.list().pipe().subscribe((data)=>{
+      this.todoList = data;
+    },error=>{
+
+    })
+  }
+
+  createTodo(){
+    let userValue = prompt("Please enter user name");
+    if (userValue != null) {
+      let todo = {
+        id: new Date().getTime(),
+        title:`New Data Created for ${userValue}` 
+      }
+      this.crudHttpService.create(todo).subscribe((response)=>this.listTodos());
+    }
+   
+  }
+
+  editTodo(todo: any){
+    let userValue = prompt("Edit user name");
+    if (userValue != null) {
+    let data = {
+      id: new Date().getTime(),
+      title:`Updated User is ${userValue}` 
+    }
+    this.crudHttpService.update(todo.id,data).subscribe((response)=>this.listTodos());
+  }
+}
+
+  deleteTodo(id: any){
+    this.crudHttpService.delete(id).subscribe((response)=> this.listTodos());
   }
 
 }
---
+++ angular-basics/src/app/services/crud-http.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudHttpService {
+
+  apiUrl: string = 'http://localhost:3000/todos';
+  //common headers
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+  constructor(private http: HttpClient) { }
+
+  // Create
+  create(data: any): Observable<any> {
+    let API_URL = `${this.apiUrl}`;
+    return this.http.post(API_URL, data);
+  }
+
+  // Read
+  list() {
+    return this.http.get(`${this.apiUrl}`);
+  }
+
+  // Update
+  update(id: any, data: any): Observable<any> {
+    let API_URL = `${this.apiUrl}/${id}`;
+    return this.http.put(API_URL, data, { headers: this.headers });
+  }
+
+  // Delete
+  delete(id: any): Observable<any> {
+    var API_URL = `${this.apiUrl}/${id}`;
+    return this.http.delete(API_URL);
+  }
+
+  // Handle API errors currently not used.
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
+
+}
